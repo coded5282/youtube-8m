b6ee42300b78fef9f84b2b2235072c56f06dcb21
coded5282 <edwardjchen@gmail.com>
2017-05-16 02:02:55 -0600

    hinge loss

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ def find_class_by_name(name, modules):
 def build_graph(reader,
                 model,
                 train_data_pattern,
-                label_loss_fn=losses.SoftmaxLoss(),
+                label_loss_fn=losses.HingeLoss(),
                 batch_size=1000,
                 base_learning_rate=0.01,
                 learning_rate_decay_examples=1000000,
