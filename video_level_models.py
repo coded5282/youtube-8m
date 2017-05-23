974b4e70340798caa4d0442e18c1e89e18362edd
coded5282 <edwardjchen@gmail.com>
2017-05-22 22:58:16 -0600

    changed to sigmoid

--- video_level_models.py
+++ video_level_models.py
@@ -50,15 +50,18 @@ class LogisticModel(models.BaseModel):
 
 class PerceptronModel(models.BaseModel):
     def create_model(self, model_input, vocab_size, l1_penalty=1e-10, **unused_params):
-        # input_layer = slim.fully_connected(model_input, 3000, activation_fn=tf.nn.relu) ## currently training
+        # input_layer = slim.fully_connected(model_input, 3000, activation_fn=tf.nn.relu) ## .65
         # hidden_layer = slim.fully_connected(input_layer, 3000, activation_fn=tf.nn.relu)
         # output = slim.fully_connected(hidden_layer, vocab_size, activation_fn=tf.nn.softmax)
 
-        # input_layer = slim.fully_connected(model_input, 2000, activation_fn=tf.nn.relu)
+        # input_layer = slim.fully_connected(model_input, 2000, activation_fn=tf.nn.relu) # training (yt8m_train_video_2000mlp)
         # hidden_layer = slim.fully_connected(input_layer, 2000, activation_fn=tf.nn.relu)
         # output = slim.fully_connected(hidden_layer, vocab_size, activation_fn=tf.nn.softmax)
 
-        hidden_layer = slim.fully_connected(model_input, 2000, activation_fn=tf.nn.relu)
+        # hidden_layer = slim.fully_connected(model_input, 2000, activation_fn=tf.nn.relu) # training (yt8m_train_video_one_layer_2000)
+        # output = slim.fully_connected(hidden_layer, vocab_size, activation_fn=tf.nn.softmax)
+
+        hidden_layer = slim.fully_connected(model_input, 2000, activation_fn=tf.nn.sigmoid) #
         output = slim.fully_connected(hidden_layer, vocab_size, activation_fn=tf.nn.softmax)
         output = tf.Print(output, [tf.argmax(output, 1)], 'out = ', summarize = 60, first_n = 100)
         return {"predictions": output}
