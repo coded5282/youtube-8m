5c3e7f3f22244418e0b286664bbc4dc104ff69e6
coded5282 <edwardjchen@gmail.com>
2017-05-23 16:33:00 -0600

    added vgg16

--- video_level_models.py
+++ video_level_models.py
@@ -66,6 +66,29 @@ class PerceptronModel(models.BaseModel):
         output = tf.Print(output, [tf.argmax(output, 1)], 'out = ', summarize = 60, first_n = 100)
         return {"predictions": output}
 
+class vgg16(models.BaseModel):
+    def create_model(self, model_input, vocab_size, l1_penalty=1e-10, **unused_params):
+        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                      activation_fn=tf.nn.relu,
+                      weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
+                      weights_regularizer=slim.l2_regularizer(0.0005)):
+            net = slim.repeat(model_input, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')
+            net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+            net = slim.max_pool2d(net, [2, 2], scope='pool2')
+            net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+            net = slim.max_pool2d(net, [2, 2], scope='pool3')
+            net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+            net = slim.max_pool2d(net, [2, 2], scope='pool4')
+            net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+            net = slim.max_pool2d(net, [2, 2], scope='pool5')
+            net = slim.fully_connected(net, 4096, scope='fc6')
+            net = slim.dropout(net, 0.5, scope='dropout6')
+            net = slim.fully_connected(net, 4096, scope='fc7')
+            net = slim.dropout(net, 0.5, scope='dropout7')
+            net = slim.fully_connected(net, vocab_size, activation_fn=None, scope='fc8')
+        return net
+
 class MoeModel(models.BaseModel):
   """A softmax over a mixture of logistic models (with L2 regularization)."""
 
