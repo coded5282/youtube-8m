c597365e834fc84d4336ddeb5d0b1daabd46314d
coded5282 <edwardjchen@gmail.com>
2017-05-25 02:37:33 -0600

    reshape

--- video_level_models.py
+++ video_level_models.py
@@ -75,6 +75,7 @@ class ConvModel(models.BaseModel):
         net = slim.conv2d(cnn_input, 128, [3])
         net = slim.pool(net, [2], "MAX")
         output = slim.fully_connected(net, vocab_size, activation_fn=tf.nn.softmax)
+        output = tf.reshape(output, [-1, vocab_size])
         return {"predictions": output}
 
 class vgg16(models.BaseModel):
