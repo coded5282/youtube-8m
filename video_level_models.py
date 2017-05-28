65423d7756bebd513744df813e7d50ca0f06c820
coded5282 <edwardjchen@gmail.com>
2017-05-28 01:29:02 -0600

    res model

--- video_level_models.py
+++ video_level_models.py
@@ -82,6 +82,17 @@ class PerceptronModel(models.BaseModel):
         output = tf.Print(output, [tf.argmax(output, 1)], 'out = ', summarize = 60, first_n = 100)
         return {"predictions": output}
 
+class ResModel(models.BaseModel):
+    def create_model(self, model_input, vocab_size, l1_penalty=1e-10, **unused_params):
+        input_layer = slim.fully_connected(model_input, 6000, activation_fn=tf.nn.relu)
+        drop_layer_1 = slim.dropout(input_layer, 0.5)
+        hidden_layer = slim.fully_connected(drop_layer_1, 6000, activation_fn=tf.nn.relu)
+        drop_layer_2 = slim.dropout(hidden_layer, 0.5)
+        skip_layer = tf.add(model_input, drop_layer_2)
+        output = slim.fully_connected(skip_layer, vocab_size, activation_fn=tf.nn.softmax)
+        return {"predictions": output}
+        pass
+
 class ConvModel(models.BaseModel):
     def create_model(self, model_input, vocab_size, l1_penalty=1e-10, **unused_params):
         # cnn_input = tf.reshape(model_input, [-1, 1024, 1])
